894013ff2cd8abedbffa41833ce5fcc0c2e4f9be
Blue Santa <contact@bluesanta.io>
2024-01-08 09:05:32 -0900

    first commit

---
+++ make_database.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Create (or update) the postgres database used to store robot data.
+
+pass=`cat .dbpass`
+
+sudo -su postgres << EOF
+
+psql << SQL
+
+ALTER USER postgres PASSWORD '$pass';
+CREATE DATABASE test_cpp;
+
+SQL
+
+EOF
+
